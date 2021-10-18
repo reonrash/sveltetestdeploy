2257b89e17485431f7136884f4acdca4a760f82a
reonrash <45687245+reonrash@users.noreply.github.com>
2021-10-18 00:02:30 -0400

    deployment

--- my-app/svelte.config.js
+++ my-app/svelte.config.js
@@ -4,7 +4,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: node()
+		adapter: node({ env: { port: process.env.PORT } })
 	}
 };
 
